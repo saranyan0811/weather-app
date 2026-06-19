057b9d17f983c834e41ebfb43a2f29735f2b06b9
saranyan0811 <saranyanarayanan38@mail.com>
2026-06-19 16:18:27 +0530

    fix Home import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import About from "./pages/About";
 import Project from "./pages/Project";
 
